46f51cbda157da2d6246afce1d663731f57c55f4
Patrick Connolly <patrick@myplanetdigital.com>
2011-12-16 22:22:48 -0500

    Added header to scripts for best-practice

--- scripts/1-commit-stage/1-build.sh
+++ scripts/1-commit-stage/1-build.sh
@@ -1,2 +1,10 @@
+#!/usr/bin/env bash
+
+# Caution is a virtue
+set -o nounset
+set -o errtrace
+set -o errexit
+set -o pipefail
+
 drush make ${PROJECT}.build build/
 cd build/ && drush site-install -y --db-url=mysql://root:${SERVER_ROOT_PASSWRD}@localhost/${PROJECT}
--- scripts/1-commit-stage/2-test.sh
+++ scripts/1-commit-stage/2-test.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
+
+# Caution is a virtue
+set -o nounset
+set -o errtrace
+set -o errexit
+set -o pipefail
+
 true
