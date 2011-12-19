4a14dee7ab3223c1414523bc34ffe2f3824ddfdb
Patrick Connolly <patrick@myplanetdigital.com>
2011-12-19 18:46:30 -0500

    Clearer automated commit msg.

--- scripts/1-commit-stage/0-pin-profile-submodule.sh
+++ scripts/1-commit-stage/0-pin-profile-submodule.sh
@@ -9,5 +9,5 @@ set -o pipefail
 git submodule foreach "git pull origin master"
 git add ${PROJECT}
 PROFILE_HASH=`git ls-remote ${PROJECT} |grep -e "[^/]HEAD" | cut -f1`
-git commit -m "Automated commit from '${PROJECT}' install profile commit ${PROFILE_HASH}"
+git commit -m "Automated commit from '${PROJECT}' install profile, commit ${PROFILE_HASH}"
 git push origin master
