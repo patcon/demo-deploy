a1cef812a6c62292fbed1f8aecbc869f9001c83e
Patrick Connolly <patrick@myplanetdigital.com>
2011-12-18 23:09:21 -0500

    Added accomodation of settings.php snippets to build script.

--- scripts/1-commit-stage/1-build.sh
+++ scripts/1-commit-stage/1-build.sh
@@ -8,3 +8,13 @@ set -o pipefail
 
 drush make ${PROJECT}.build build/
 cd build/ && drush site-install -y --db-url=mysql://root:${SERVER_ROOT_PASSWORD}@localhost/${PROJECT}
+
+# Append settings.php snippets
+chmod u+w ${WORKSPACE}/build/sites/default/settings.php
+cd ${WORKSPACE}/build/profiles/${PROJECT}/includes
+for f in *.settings.php
+do
+  echo "" >> ${WORKSPACE}/build/sites/default/settings.php
+  cat $f  >> ${WORKSPACE}/build/sites/default/settings.php
+done
+chmod u-w ${WORKSPACE}/build/sites/default/settings.php
