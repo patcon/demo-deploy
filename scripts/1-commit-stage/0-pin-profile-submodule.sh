c254e1a97ac86d26a50634d44e7c19b40e24d1ea
Patrick Connolly <patrick@myplanetdigital.com>
2011-12-19 18:54:33 -0500

    Trying again to fix 0-pin... script -- git checkout

--- scripts/1-commit-stage/0-pin-profile-submodule.sh
+++ scripts/1-commit-stage/0-pin-profile-submodule.sh
@@ -7,6 +7,7 @@ set -o errexit
 set -o pipefail
 
 git submodule foreach "git pull origin master"
+git checkout master
 git add ${PROJECT}
 PROFILE_HASH=`git ls-remote ${PROJECT} |grep -e "[^/]HEAD" | cut -f1`
 git commit -m "Automated commit from '${PROJECT}' install profile, commit ${PROFILE_HASH}"
