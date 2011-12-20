d6821c063055c31a1370e1caf733e4f5149d1722
Patrick Connolly <patrick@myplanetdigital.com>
2011-12-19 19:11:42 -0500

    Silence failures for commit when nothign staged in 0-pin..

--- scripts/1-commit-stage/0-pin-profile-submodule.sh
+++ scripts/1-commit-stage/0-pin-profile-submodule.sh
@@ -10,5 +10,5 @@ git submodule foreach "git pull origin master"
 git checkout master
 git add ${PROJECT}
 PROFILE_HASH=`git ls-remote ${PROJECT} |grep -e "[^/]HEAD" | cut -f1`
-git commit -m "Automated commit from '${PROJECT}' install profile, commit ${PROFILE_HASH}"
+git commit -m "Automated commit from '${PROJECT}' install profile, commit ${PROFILE_HASH}" | true
 git push origin master
